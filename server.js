412d38f8b4986c12f0fdc923fb68ae8a7edcc810
Cordova <cordovansyah@gmail.com>
2022-11-09 12:01:52 +0700

    Improve folder structure for Vercel Deployment

--- server.js
+++ server.js
@@ -2,16 +2,12 @@ const express = require('express');
 const app = express();
 const http = require('http')
 const server = http.createServer(app)
-const port = 3000;
-
-var io = require('socket.io')(server);
-app.use(express.static('public'));
 
 // Listen to Static Files
-app.use('/css', express.static(__dirname + 'public/css'));
-app.use('/javascript', express.static(__dirname + 'public/javascript'));
-app.use('/image', express.static(__dirname + 'public/image'));
-
+app.use(express.static('public'));
+// app.use('/css', express.static(__dirname + 'public/css'));
+// app.use('/javascript', express.static(__dirname + 'public/javascript'));
+// app.use('/image', express.static(__dirname + 'public/image'));
 
 // Listen to HTML
 app.get('', (req, res) => {
