6ee7cd798d0e7e58b7ced6a4f9a3ef5f7811a285
Cordova <cordovansyah@gmail.com>
2022-11-17 08:41:16 +0700

    test venti model for mobile

--- public/javascript/zhongli.js
+++ public/javascript/zhongli.js
@@ -317,7 +317,7 @@ function move() {
   const loaderZhongli = new THREE.GLTFLoader();
   loaderZhongli.crossOrigin = "anonymous";
   loaderZhongli.load(
-    "https://cdn.glitch.me/40a5d633-e9a4-4101-b267-6437f92dd8d8/Zhongli.vrm?v=1668411077094",
+    "https://cdn.glitch.me/40a5d633-e9a4-4101-b267-6437f92dd8d8/Venti%20v1.5.vrm?v=1668649248708",
     gltf => {
       THREE.VRMUtils.removeUnnecessaryJoints(gltf.scene);
   
